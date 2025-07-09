be7a682459a2f16f3fe9bff83368c2f98042c67b
蓝田_Loto <shorfng@126.com>
2025-07-09 17:31:41 +0800

    refactor: 优化 OpenApiConfig 类中的代码结构 - 移除了不必要的临时变量 contact - 使用链式调用简化了 Contact 对象的创建和赋值 - 通过直接在 Info 对象中设置 contact，提高了代码的可读性和简洁性

--- src/main/java/com/loto/config/OpenApiConfig.java
+++ src/main/java/com/loto/config/OpenApiConfig.java
@@ -15,15 +15,11 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
     @Bean
     public OpenAPI springOpenAPI() {
-        io.swagger.v3.oas.models.info.Contact contact = new io.swagger.v3.oas.models.info.Contact()
-                .name("蓝田_Loto")
-                .email("shorfng@126.com");
-
         return new OpenAPI()
                 .info(new Info()
                         .title("JavaAPI_SpringBoot")
                         .description("Java接口项目模版_SpringBoot_适用于Java 8")
-                        .contact(contact)
+                        .contact(new io.swagger.v3.oas.models.info.Contact().name("蓝田_Loto"))
                         .version("1.0.0"));
     }
 }
